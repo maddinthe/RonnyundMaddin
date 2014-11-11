323d61b827ebd4193e7c6a3de15b55a87c22a36e
maddinthe <martintheilen@gmail.com>
2014-11-11 09:31:01 +0100

    Rechtschreibkorrektur in den Comments

--- Strings/src/Reverse.java
+++ Strings/src/Reverse.java
@@ -5,40 +5,40 @@ public class Reverse {
 
 
 	public String reverseWordByWord(String text) {
-		int strLength = text.length() - 1; 								// setzt die Variable Stringlänge
+		int strLength = text.length() - 1; 								// setzt die Variable Stringlaenge
 		String reverse = "", temp = "";									// initialisiert reverse und temp als
 																		// String
 
-		for (int i = 0; i <= strLength; i++) { 							// läuft so lange bis I dem Wert
-																		// der Variable Stringlänge
+		for (int i = 0; i <= strLength; i++) { 							// laeuft so lange bis I dem Wert
+																		// der Variable Stringlaenge
 																		// entspricht
-			if (text.charAt(i) != ' ') { 								// prüft ob kein Leerzeichen an
+			if (text.charAt(i) != ' ') { 								// prueft ob kein Leerzeichen an
 																		// Position I im String text
 																		// gefunden wurde
-				temp += text.charAt(i); 								// und fügt dann den Char der position i
+				temp += text.charAt(i); 								// und fuegt dann den Char der position i
 																		// hinten an den String temp an
 			}
-			if (text.charAt(i) == ' ') {								// prüft ob ein Leerzeichen an pos i im
+			if (text.charAt(i) == ' ') {								// prueft ob ein Leerzeichen an pos i im
 																		// String text vorhanden ist
 				for (int j = temp.length() - 1; j >= 0; j--) {
 					reverse += temp.charAt(j);
 					if (j == 0) {
-						reverse += " "; 								// fügt den string temp Zeichenweise,
+						reverse += " "; 								// fuegt den string temp Zeichenweise,
 																		// von hinten nach vorne, an den string
-																		// reverse an und fügt am Ende ein
+																		// reverse an und fuegt am Ende ein
 																		// Leerzeichen ein
 					}
 				}
-				temp = ""; 												// setzt temp wieder auf leer zurück
+				temp = ""; 												// setzt temp wieder auf leer zurueck
 			} else if (i == strLength) {
 				for (int j = temp.length() - 1; j >= 0; j--) {
-					reverse += temp.charAt(j);							// prüft ob i der stringlänge
-																		// entspricht und fügt den
+					reverse += temp.charAt(j);							// prueft ob i der stringlaenge
+																		// entspricht und fuegt den
 																		// string temp Zeichenweise, von
 																		// hinten nach vorne, an den
 																		// string reverse an
 				}
-				temp = ""; 												// setzt temp zurück
+				temp = ""; 												// setzt temp zurueck
 			}
 		}
 

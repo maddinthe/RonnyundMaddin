5b568e598d753805bb7a023b269e918bcddb6ec5
maddinthe <martintheilen@gmail.com>
2014-11-26 09:30:53 +0100

    first commit

---
+++ Taschenrechner/src/Segmentanzeige.java
@@ -0,0 +1,19 @@
+
+public class Segmentanzeige {
+	boolean[] anzeige=new boolean[63];
+	
+	public void an (int seg){
+		anzeige[seg]=true;
+	}
+	public void aus (int seg){
+		anzeige[seg]=false;
+	}
+	public boolean isAn(int seg){		
+		return anzeige[seg];
+	}
+	public void clearAll(){
+		for (int i=0;i<anzeige.length;i++)
+			anzeige[i]=false;
+		
+	}
+}

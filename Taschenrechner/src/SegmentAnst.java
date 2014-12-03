abbe58e4ff28d76ace1d25168b5b59ef4a25cb20
maddinthe <martintheilen@gmail.com>
2014-12-03 13:37:03 +0100

    fertscvh

--- Taschenrechner/src/SegmentAnst.java
+++ Taschenrechner/src/SegmentAnst.java
@@ -8,7 +8,9 @@ public class SegmentAnst {
 	private int[][] ansteuer = {{1,5,7,3,6,4},{5,7},{1,5,2,6,3},{1,5,2,7,3},{4,5,2,7},{1,4,2,7,3},{1,4,2,7,3,6},{1,5,7},{1,2,3,4,5,6,7},{1,4,5,2,7,3}};
 	
 	public void anst(double AusgWert){
+		AusgWert+=0.00000000001;
 		String zahl=String.valueOf(AusgWert);
+		
 		if (zahl.contains("-")&zahl.contains("."))
 			zahl=zahl.substring(0,10);
 		else if (zahl.contains("-")||zahl.contains("."))
--- Taschenrechner/src/tester.java
+++ Taschenrechner/src/tester.java
@@ -5,7 +5,7 @@ public class tester {
 		// TODO Auto-generated method stub
 		SegmentAnst seganst=new SegmentAnst();
 		Segmentanzeige seg=seganst.getSeg();
-		seganst.anst(-12.34567890);
+		seganst.anst(-12.34);
 		for (int i=0;i<64;i++){
 			System.out.print(seg.isAn(i));
 			System.out.println(" Element"+ i);

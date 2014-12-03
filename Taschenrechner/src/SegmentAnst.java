0d930d4351ccaf7c6aed118db7637b0f63a080d7
maddinthe <martintheilen@gmail.com>
2014-12-03 13:55:21 +0100

    noch fertiger

--- Taschenrechner/src/SegmentAnst.java
+++ Taschenrechner/src/SegmentAnst.java
@@ -8,10 +8,9 @@ public class SegmentAnst {
 	private int[][] ansteuer = {{1,5,7,3,6,4},{5,7},{1,5,2,6,3},{1,5,2,7,3},{4,5,2,7},{1,4,2,7,3},{1,4,2,7,3,6},{1,5,7},{1,2,3,4,5,6,7},{1,4,5,2,7,3}};
 	
 	public void anst(double AusgWert){
-		AusgWert+=0.00000000001;
-		String zahl=String.valueOf(AusgWert);
+		String zahl=String.valueOf(AusgWert)+"                ";
 		
-		if (zahl.contains("-")&zahl.contains("."))
+		if (zahl.contains("-")&&zahl.contains("."))
 			zahl=zahl.substring(0,10);
 		else if (zahl.contains("-")||zahl.contains("."))
 			zahl=zahl.substring(0,9);
@@ -25,12 +24,13 @@ public class SegmentAnst {
 				seg.an(j);
 				break;
 			default:
+				if (Character.isDigit(zahl.charAt(i))){
 				l=Character.getNumericValue(zahl.charAt(i));
 				for (int k=0;k<ansteuer[l].length;k++){
 					seg.an(j+(ansteuer[l][k]));
 				}
 				j+=8;
-				
+				}
 			}
 		}
 		
